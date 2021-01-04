5e793e466c2ca2a21e3d68ff68a3b07d35f4c027
Kito Cheng <kito.cheng@sifive.com>
2021-01-04 15:07:43 +0800

    Bump version to 2020.12.1

--- Version.mk
+++ Version.mk
@@ -2,7 +2,7 @@
 RISCV_GCC_VERSION := 10.2.0
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_GCC_METAL_ID := 2020.12.2-rc1
+FREEDOM_GCC_METAL_ID := 2020.12.1
 
 # Characteristic tags, which should be usable for matching up providers and consumers
 FREEDOM_GCC_METAL_RISCV_TAGS = rv32i rv64i m a f d c v zfh
