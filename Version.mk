2674bc0e8cce1e2e6eb97cadfb8a7e29ac893035
cgsfv <cgsfv@users.noreply.github.com>
2021-01-19 12:43:29 -0800

    Bumping package version number

--- Version.mk
+++ Version.mk
@@ -2,8 +2,8 @@
 RISCV_GCC_VERSION := 10.2.0
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_GCC_METAL_ID := 2020.12.5
+FREEDOM_GCC_METAL_ID := 2020.12.7
 
 # Characteristic tags, which should be usable for matching up providers and consumers
-FREEDOM_GCC_METAL_RISCV_TAGS = rv32i rv64i m a f d c v zfh
+FREEDOM_GCC_METAL_RISCV_TAGS = rv32i rv64i m a f d c v b zfh
 FREEDOM_GCC_METAL_TOOLS_TAGS = gcc10-metal
