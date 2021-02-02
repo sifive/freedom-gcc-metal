72dece1e6b0bf83ccfb09ffbd7c5708f4f6cd159
Kito Cheng <kito.cheng@sifive.com>
2021-02-02 18:50:19 +0800

    Auto version for dev

--- Version.mk
+++ Version.mk
@@ -2,7 +2,7 @@
 RISCV_GCC_VERSION := 10.2.0
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_GCC_METAL_ID := 2020.12.8
+FREEDOM_GCC_METAL_ID := dev-$(shell cd src/riscv-gcc/ && git log --pretty=format:'%h' -1)-$(shell cd src/riscv-newlib/ && git log --pretty=format:'%h' -1)
 
 # Characteristic tags, which should be usable for matching up providers and consumers
 FREEDOM_GCC_METAL_RISCV_TAGS = rv32i rv64i m a f d c v zfh zba zbb
